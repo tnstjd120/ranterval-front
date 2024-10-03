184b4794422514b6a4602e2654c369b3a3270b80
tnstjd120 <tnstjd120@gmail.com>
2024-10-04 01:18:43 +0900

    chore: rewrites /api/v1/:path*

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: '/api/v1/:path*',
+      destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/:path*`,
+    },
+  ],
 };
 
 export default nextConfig;
